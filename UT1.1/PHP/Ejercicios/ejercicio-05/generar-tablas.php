6148e60945ce511cd1fa8aa3cfd1274e5144c3e8
José Luis López Álvarez <jllopezalvarez@educa.madrid.org>
2024-10-10 19:18:55 +0200

    Cambios en Ejercicios

--- UT1.1/PHP/Ejercicios/ejercicio-05/generar-tablas.php
+++ UT1.1/PHP/Ejercicios/ejercicio-05/generar-tablas.php
@@ -1,14 +1,14 @@
 <table class="table table-border table-striped-columns">
     <tr>
         <td></td>
-        <?php for ($i = $numInicial; $i <= $numFinal; $i++): ?>
+        <?php for ($i = $inicio; $i <= $fin; $i++): ?>
             <th scope="col"><?= $i ?></th>
         <?php endfor; ?>
     </tr>
     <?php for ($fila = 0; $fila < 10; $fila++): ?>
         <tr>
             <th scope="row"><?= $fila ?></th>
-            <?php for ($i = $numInicial; $i <= $numFinal; $i++): ?>
+            <?php for ($i = $inicio; $i <= $fin; $i++): ?>
                 <td><?= $fila * $i ?></td>
             <?php endfor; ?>
 
